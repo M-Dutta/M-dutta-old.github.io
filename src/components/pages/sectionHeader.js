1895d0a9dea4895ea6992dae9fb47e7ad4cf765b
M-Dutta <dd31137@uga.edu>
2020-06-13 04:35:39 -0400

    centered all and fixed switch

--- src/components/pages/sectionHeader.js
+++ src/components/pages/sectionHeader.js
@@ -9,18 +9,17 @@ const secondarySectionHeaderStyle = {
 }
 
 const SectionHeader = props => {
-    console.log(props)
     return (
-        <div className='container'>
-            <h1 className='row justify-content-start section-header mb-5' style={sectionHeaderStyle}>{props.children}</h1>
-        </div>
+    <div className='container'>
+     <h1 className='row justify-content-center section-header mb-5' style={sectionHeaderStyle}>{props.children}</h1>
+     </div>
     )
 }
 
 const SecondaryHeader = props => {
     return (
-        <div>
-            <h2 className='row secondary-section-header'style={secondarySectionHeaderStyle}>{props.children}</h2>
+        <div className='container'>
+            <h2 className='row justify-content-center secondary-section-header' style={secondarySectionHeaderStyle}>{props.children}</h2>
         </div>
     )
 }
