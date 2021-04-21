4a7bef0dc2869e9609d5920082548182271abf53
M-Dutta <mishukdtt@live.com>
2021-04-21 19:47:00 -0400

    Updates

---
+++ precache-manifest.55e5d124356ee10420720936742ee2fa.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "291a3377f812f7c52d4a34b095ab4a7d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca16ac4b14264b6ee8c6",
+    "url": "/static/css/2.be04442c.chunk.css"
+  },
+  {
+    "revision": "4a502b5654dc35ff2288",
+    "url": "/static/css/main.b3bdc077.chunk.css"
+  },
+  {
+    "revision": "ca16ac4b14264b6ee8c6",
+    "url": "/static/js/2.95a59362.chunk.js"
+  },
+  {
+    "revision": "a2579ed7df986a64f9f2f96b6a2daefd",
+    "url": "/static/js/2.95a59362.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a502b5654dc35ff2288",
+    "url": "/static/js/main.18466836.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "402c850c0bd64b0cb00fcf726dd2db60",
+    "url": "/static/media/Mishuk-resume.402c850c.pdf"
+  },
+  {
+    "revision": "8b83cdfeaf6decc5502f1ff273cca338",
+    "url": "/static/media/me.8b83cdfe.jpg"
+  }
+]);
--- src/components/canvas/particles/Particles.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Particles from 'react-particles-js';
-
-export const BGParticles = () => {
-	const particleOptions  = require('./particle.json');
-	return (
-		<Particles params={particleOptions} />
-	)
-}
