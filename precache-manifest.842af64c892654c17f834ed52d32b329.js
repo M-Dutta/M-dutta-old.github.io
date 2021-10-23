9b92b305b523a3d0f570eda6721badaf90ab5dbc
M-Dutta <mishukdtt@live.com>
2021-10-23 04:12:57 -0400

    Updates

---
+++ precache-manifest.842af64c892654c17f834ed52d32b329.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d8e1fcbbd4acbf93f558f914c4cc2c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "39ea0499add4b7326adc",
+    "url": "/static/css/2.be04442c.chunk.css"
+  },
+  {
+    "revision": "3b805245ad1261c22e1c",
+    "url": "/static/css/main.b3bdc077.chunk.css"
+  },
+  {
+    "revision": "39ea0499add4b7326adc",
+    "url": "/static/js/2.98c53f69.chunk.js"
+  },
+  {
+    "revision": "a2579ed7df986a64f9f2f96b6a2daefd",
+    "url": "/static/js/2.98c53f69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b805245ad1261c22e1c",
+    "url": "/static/js/main.bbc7a526.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "402c850c0bd64b0cb00fcf726dd2db60",
+    "url": "/static/media/Mishuk-resume.402c850c.pdf"
+  },
+  {
+    "revision": "338562efcc31ff755e66b7867c4914b1",
+    "url": "/static/media/me.338562ef.jpg"
+  }
+]);
